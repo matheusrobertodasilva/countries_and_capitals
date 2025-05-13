25d3b002eb6766d7940c300cd27fd2782922e9c2
matheusrobertodasilva <matheusrobertodasilva16@gmail.com>
2025-05-13 17:19:13 -0300

    004

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::view ('/', 'home');
+// start game
+Route::get('/', [MainController::class, 'startGame'])->name('start_game');
+Route::post('/', [MainController::class, 'prepareGame'])->name('prepare_game');
 
-Route::get('/show_data', [MainController::class, 'showData']);
