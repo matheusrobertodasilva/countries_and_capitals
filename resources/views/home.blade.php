a8f9d9a681ab134f80f98863a0aba7d520134f4e
matheusrobertodasilva <matheusrobertodasilva16@gmail.com>
2025-05-13 16:52:57 -0300

    003

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+<x-main-layout pageTitle="Countries & Capitals Quiz">
+
+    <h1 class="text-center my-5">HOME PAGE</h1>
+
+</x-main-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    echo 'Hello world';
-});
+Route::view ('/', 'home');
 
 Route::get('/show_data', [MainController::class, 'showData']);
