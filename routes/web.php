791342fef54ac9f9da06c8de285bcfb8f670729f
matheusrobertodasilva <matheusrobertodasilva16@gmail.com>
2025-05-16 10:33:10 -0300

    008

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,5 @@ Route::post('/', [MainController::class, 'prepareGame'])->name('prepare_game');
 
 // in game
 Route::get('/game', [MainController::class, 'game'])->name('game');
+Route::get('/answer/{answer}', [MainController::class, 'answer'])->name('answer');
+
